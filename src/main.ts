8f870893e0f84b0ae8d1916b70b60cec3365803c
Na1gae <nalgae@korea.ac.kr>
2025-08-06 15:44:33 +0900

    feat(docs): Swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
+
+  const config = new DocumentBuilder()
+    .setTitle('C:LEC API')
+    .setDescription('CLEC API')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
