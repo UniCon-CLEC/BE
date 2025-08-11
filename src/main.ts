2594c51c98e7b2ec5136146887e0f3382a57046e
Na1gae <nalgae@korea.ac.kr>
2025-08-11 16:03:02 +0900

    fix(infra): Cors 활성화

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService)
-  await prismaService.enableShutdownHooks(app)
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://clec.nalgae.me',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   const config = new DocumentBuilder()
     .setTitle('C:LEC API')
