dabebd3b323b0f73ba1b25d0249edd815ead1769
Na1gae <nalgae@korea.ac.kr>
2025-08-24 01:01:56 +0900

    chore: 파일명 컨벤션 통일

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './module';
 import { PrismaService } from './prisma/prisma.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
--- src/module.ts
+++ src/module.ts
@@ -1,23 +1,29 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller';
+import { AppService } from './service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/module';
 import { TagModule } from './tag/module';
 import { MeModule } from './me/module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { TrackModule } from './track/module';
+import { CrewModule } from './crew/module';
+import { InstructorModule } from './instructor/instructor.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env'
+      envFilePath: '.env',
     }),
     PrismaModule,
     AuthModule,
     TagModule,
-    MeModule
+    MeModule,
+    TrackModule,
+    CrewModule,
+    InstructorModule,
   ],
   controllers: [AppController],
   providers: [AppService],
