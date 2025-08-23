ded8eaa7270236177b7ea0084185f775c228a402
Na1gae <nalgae@korea.ac.kr>
2025-08-24 02:07:59 +0900

    feat: 내 정보 페이지에 트랙, 크루, 펀딩내역 추가

--- src/me/dto.ts
+++ src/me/dto.ts
@@ -1,72 +1,88 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { CourseStatus, EnrollmentStatus } from '@prisma/client';
-
-export class UpdateMeDto {
-  
-}
+import { CourseType, CrewStatus, EnrollmentStatus, TrackStatus } from '@prisma/client';
 
 export class InstructorInfoDto {
-    @ApiProperty({ description: '강사 ID' })
-    id: string
+  @ApiProperty({ description: '강사 ID' })
+  id: string;
 
-    @ApiProperty({ description: '강사명' })
-    name: string
+  @ApiProperty({ description: '강사명' })
+  name: string;
 
-    @ApiProperty({ description: '강사 이미지 URL' })
-    image?: string
-
-    @ApiProperty({ description: '강사 스케쥴 (표 형태의 Table로 제공?)' })
-    schedule?: string
-
-    @ApiProperty({ description: '강사 간단 소개' })
-    information?: string
+  @ApiProperty({ description: '강사 이미지 URL' })
+  image?: string;
 }
 
-export class EnrolledCourseDto {
-    @ApiProperty({ description: '강의 ID' })
-    courseId: string;
+export class MyFundingDto {
+  @ApiProperty({ description: '코스 ID' })
+  courseId: string;
 
-    @ApiProperty({ description: '강의 제목' })
-    title: string;
+  @ApiProperty({ description: '코스 제목' })
+  title: string;
 
-    @ApiProperty({ description: '강의 상태 (펀딩중, 진행중 등)', enum: CourseStatus })
-    courseStatus: CourseStatus;
+  @ApiProperty({ description: '펀딩 상태', enum: EnrollmentStatus })
+  status: EnrollmentStatus;
 
-    @ApiProperty({ description: '강의 대표 이미지 URL', nullable: true })
-    coverImageUrl: string | null;
+  @ApiProperty({ description: '내가 펀딩한 금액' })
+  amountPaid: number;
 
-    @ApiProperty({ description: '강사 정보', nullable: true })
-    instructor: InstructorInfoDto;
+  @ApiProperty({ description: '펀딩 시작일' })
+  fundingStartDate: Date;
 
-    @ApiProperty({ description: '나의 수강 상태 (결제완료, 펀딩참여 등)', enum: EnrollmentStatus })
-    enrollmentStatus: EnrollmentStatus;
+  @ApiProperty({ description: '펀딩 종료일' })
+  fundingEndDate: Date;
+}
 
-    @ApiProperty({ description: '펀딩 진행률 / 금액 (%)' })
-    fundingProgress: number;
+export class MyCourseDto {
+  @ApiProperty({ description: '코스 ID' })
+  courseId: string;
 
-    @ApiProperty({ description: '펀딩 시작일' })
-    fundingStartDate: Date;
+  @ApiProperty({ description: '코스 타입', enum: CourseType })
+  type: CourseType;
 
-    @ApiProperty({ description: '펀딩 종료일' })
-    fundingEndDate: Date;
+  @ApiProperty({ description: '코스 제목' })
+  title: string;
 
-    @ApiProperty({ description: '강의 시작일' })
-    courseStartDate: Date;
+  @ApiProperty({ description: '강의 대표 이미지 URL', nullable: true })
+  coverImageUrl: string | null;
+
+  @ApiProperty({ description: '강사 정보' })
+  instructor: InstructorInfoDto;
+
+  @ApiProperty({ description: '트랙 상태', enum: TrackStatus, required: false })
+  trackStatus?: TrackStatus;
+
+  @ApiProperty({ description: '크루 상태', enum: CrewStatus, required: false })
+  crewStatus?: CrewStatus;
+
+  @ApiProperty({ description: '진행 회차', required: false })
+  currentRound?: number;
+
+  @ApiProperty({ description: '펀딩률', required: false })
+  fundingRate?: number;
+
+  @ApiProperty({ description: '펀딩 금액', required: false })
+  fundedAmount?: number;
+
+  @ApiProperty({ description: '나의 펀딩 상태', enum: EnrollmentStatus, required: false })
+  myFundingStatus?: EnrollmentStatus;
 }
 
 export class MeResultDto {
-    @ApiProperty({ description: '사용자 ID' })
-    id: string;
+  @ApiProperty({ description: '사용자 ID' })
+  id: string;
 
-    @ApiProperty({ description: '사용자 이름', nullable: true })
-    name: string | null;
+  @ApiProperty({ description: '사용자 이름', nullable: true })
+  name: string | null;
 
-    @ApiProperty({ description: '프로필 이미지 URL', nullable: true })
-    profileImageUrl: string | null;
+  @ApiProperty({ description: '프로필 이미지 URL', nullable: true })
+  profileImageUrl: string | null;
 
-    @ApiProperty({ description: '사용자 관심 태그 목록' })
-    tags: string[];
+  @ApiProperty({ description: '사용자 관심 태그 목록' })
+  tags: string[];
 
-    @ApiProperty({ description: '사용자가 등록한 강의 목록', type: [EnrolledCourseDto] })
-    enrolledCourses: EnrolledCourseDto[];
+  @ApiProperty({ description: '수강중인 코스 목록', type: [MyCourseDto] })
+  courses: MyCourseDto[];
+
+  @ApiProperty({ description: '나의 펀딩 목록', type: [MyFundingDto] })
+  fundings: MyFundingDto[];
 }
--- src/me/service.ts
+++ src/me/service.ts
@@ -1,67 +1,140 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { MeResultDto, UpdateMeDto } from './dto';
+import { MeResultDto, MyCourseDto, MyFundingDto } from './dto';
 import { Decimal } from '@prisma/client/runtime/library';
+import { CourseForSort, CrewEnrollmentForDto, TrackEnrollmentForDto, UserWithEnrollments } from './types';
 
 @Injectable()
 export class MeService {
   constructor(private prisma: PrismaService) {}
 
   async getMe(userId: string): Promise<MeResultDto> {
-    const user = await this.prisma.user.findUnique({
+    const user = await this.fetchUserWithEnrollments(userId)
+
+    const trackCourses = user.trackEnrollments.map((e) =>
+      this.transformTrackToCourseDto(e)
+    )
+    const crewCourses = user.crewEnrollments.map((e) =>
+      this.transformCrewToCourseDto(e)
+    )
+    const fundings = user.trackEnrollments.map((e) =>
+      this.transformTrackToFundingDto(e)
+    )
+
+    const courses: MyCourseDto[] = [...trackCourses, ...crewCourses]
+
+    return {
+      id: user.id,
+      name: user.name,
+      profileImageUrl: user.profileImageUrl,
+      tags: user.tags.map((tag) => tag.name),
+      courses,
+      fundings
+    }
+  }
+
+  private fetchUserWithEnrollments(userId: string): Promise<UserWithEnrollments>{
+    const courseSelection = {
+      id: true, 
+      type: true, title: true, courseStartDate: true,
+      instructor: true,
+      introduction: { select: { coverImageUrl: true } }
+    }
+
+    return this.prisma.user.findUnique({
       where: { id: userId },
       include: {
         tags: { select: { name: true } },
-        enrollments: { 
-          include: { 
-            course: {
-              include: {
-                introduction: { select: { coverImageUrl: true } },
-                instructor: { select: { information: true, name: true, id: true, image: true, schedule: true } },
-                enrollments: { where: { status: 'PAID'}, select: { amountPaid: true } },
+        trackEnrollments: {
+          select: {
+            status: true, createdAt: true, amountPaid: true,
+            track: {
+              select: {
+                status: true, fundingTargetAmount: true, fundingStartDate: true, fundingEndDate: true, courseId: true,
+                course: { select: courseSelection },
+                enrollments: { where: { status: 'PAID' }, select: { amountPaid: true } }
+              }
+            }
+          }
+        },
+        crewEnrollments: {
+          select: {
+            status: true, createdAt: true,
+            crew: {
+              select: {
+                status: true,
+                course: { select: courseSelection }
               }
             }
           }
         }
       }
     })
+  }
 
-    // TODO: Apply Redis Cache by Using External PrivateFunction
-    const enrolledCourses = user.enrollments.map((e) => {
-      const { course } = e
-      const currentFundingAmount = course.enrollments.reduce(
-        (s, e) => s.add(e.amountPaid),
-        new Decimal(0)
+  private transformTrackToCourseDto(enrollment: TrackEnrollmentForDto): CourseForSort {
+    const { track, status: myFundingStatus, createdAt } = enrollment
+    const { course } = track
+
+    const currentFundingAmount = track.enrollments.reduce(
+      (sum, en) => sum.add(en.amountPaid), new Decimal(0),
+    )
+
+    const targetAmount = track.fundingTargetAmount
+    let fundingProgress = 0
+    if (targetAmount.greaterThan(0)) {
+      fundingProgress = Math.round(
+        currentFundingAmount.div(targetAmount).toNumber() * 100,
       )
-      const targetAmount = course.fundingTargetAmount
-      let fundingProgress = 0
-      if (targetAmount.greaterThan(0))
-        fundingProgress = Math.round(
-          currentFundingAmount.div(targetAmount).toNumber() * 100
-      );
-
-      return {
-        courseId: course.id,
-        title: course.title,
-        courseStatus: course.status,
-        coverImageUrl: course.introduction?.coverImageUrl ?? null,
-        instructor: course.instructor,
-        enrollmentStatus: e.status,
-        fundingProgress: fundingProgress,
-        fundingStartDate: course.fundingStartDate,
-        fundingEndDate: course.fundingEndDate,
-        courseStartDate: course.courseStartDate
-      }
-    })
-
-    const res: MeResultDto = {
-      id: user.id,
-      name: user.name,
-      profileImageUrl: user.profileImageUrl,
-      tags: user.tags.map((tag) => tag.name),
-      enrolledCourses: enrolledCourses
     }
+    
+    return {
+      courseId: course.id,
+      type: course.type,
+      title: course.title,
+      coverImageUrl: course.introduction?.coverImageUrl ?? null,
+      instructor: course.instructor,
+      trackStatus: track.status,
+      fundingRate: fundingProgress,
+      fundedAmount: currentFundingAmount.toNumber(),
+      myFundingStatus,
+      createdAt
+    };
+  }
 
-    return res 
+  private transformCrewToCourseDto(enrollment: CrewEnrollmentForDto): CourseForSort {
+    const { crew, status: myEnrollmentStatus, createdAt } = enrollment
+    const { course } = crew
+
+    const now = new Date()
+    const courseStartDate = new Date(course.courseStartDate)
+    const weeksPassed = Math.floor(
+      (now.getTime() - courseStartDate.getTime()) / (1000 * 60 * 60 * 24 * 7),
+    )
+    const ongoingSession = weeksPassed > 0 ? weeksPassed : 0
+    
+    return {
+      courseId: course.id,
+      type: course.type,
+      title: course.title,
+      coverImageUrl: course.introduction?.coverImageUrl ?? null,
+      instructor: course.instructor,
+      crewStatus: crew.status,
+      currentRound: ongoingSession,
+      myFundingStatus: myEnrollmentStatus,
+      createdAt
+    }
+  }
+
+  private transformTrackToFundingDto(enrollment: TrackEnrollmentForDto): MyFundingDto {
+    const { track, status, amountPaid } = enrollment
+    return {
+      courseId: track.courseId,
+      title: track.course.title,
+      status,
+      amountPaid: amountPaid.toNumber(),
+      fundingStartDate: track.fundingStartDate,
+      fundingEndDate: track.fundingEndDate
+    }
   }
 }
