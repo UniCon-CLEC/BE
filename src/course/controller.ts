f2b070df94058e02cc50af3c159303c878bd0f78
Na1gae <nalgae@korea.ac.kr>
2025-08-31 22:14:51 +0900

    feat(course): 변경된 데이터베이스에 맞춘 데이터 타입 정의

--- src/course/controller.ts
+++ src/course/controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  NotFoundException, 
+  Param 
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CourseService } from './service';
-import { TrackDto, CrewDto, CourseSessionDto } from './dto';
+import {
+  TrackCourseDto,
+  CrewCourseDto,
+  FundingCourseDto,
+} from './dto';
 
 @ApiTags('Course (코스)')
 @Controller('courses')
@@ -10,23 +19,30 @@ export class CourseController {
 
   @Get('track')
   @ApiOperation({ summary: '모든 트랙 목록 조회' })
-  @ApiResponse({ status: 200, description: '성공', type: [TrackDto] })
+  @ApiResponse({ status: 200, description: '성공', type: [TrackCourseDto] })
   getAllTracks() {
     return this.courseService.getAllTracks();
   }
 
   @Get('crew')
   @ApiOperation({ summary: '모든 크루 목록 조회' })
-  @ApiResponse({ status: 200, description: '성공', type: [CrewDto] })
+  @ApiResponse({ status: 200, description: '성공', type: [CrewCourseDto] })
   getAllCrews() {
     return this.courseService.getAllCrews();
   }
 
+  @Get('funding')
+  @ApiOperation({ summary: '모든 펀딩 목록 조회' })
+  @ApiResponse({ status: 200, description: '성공', type: [FundingCourseDto] })
+  getAllFundings() {
+    return this.courseService.getAllFundings();
+  }
+
   @Get('track/:courseId')
   @ApiOperation({ summary: '특정 트랙 상세 조회' })
-  @ApiResponse({ status: 200, description: '성공', type: TrackDto })
+  @ApiResponse({ status: 200, description: '성공', type: TrackCourseDto })
   @ApiResponse({ status: 404, description: '트랙을 찾을 수 없음' })
-  async getTrackById(@Param('courseId') courseId: string) {
+  async getTrackByCourseId(@Param('courseId') courseId: string) {
     const track = await this.courseService.getTrackByCourseId(courseId);
     if (!track) {
       throw new NotFoundException(`Track with course ID ${courseId} not found`);
@@ -36,9 +52,9 @@ export class CourseController {
 
   @Get('crew/:courseId')
   @ApiOperation({ summary: '특정 크루 상세 조회' })
-  @ApiResponse({ status: 200, description: '성공', type: CrewDto })
+  @ApiResponse({ status: 200, description: '성공', type: CrewCourseDto })
   @ApiResponse({ status: 404, description: '크루를 찾을 수 없음' })
-  async getCrewById(@Param('courseId') courseId: string) {
+  async getCrewByCourseId(@Param('courseId') courseId: string) {
     const crew = await this.courseService.getCrewByCourseId(courseId);
     if (!crew) {
       throw new NotFoundException(`Crew with course ID ${courseId} not found`);
@@ -46,21 +62,15 @@ export class CourseController {
     return crew;
   }
 
-  // TODO: Add AuthGuard for check permission
-  @Get('track/:courseId/sessions')
-  @ApiOperation({ summary: '특정 트랙의 강의 세션 목록 조회' })
-  @ApiResponse({ status: 200, description: '성공', type: [CourseSessionDto] })
-  @ApiResponse({ status: 404, description: '트랙을 찾을 수 없음' })
-  getTrackSessions(@Param('courseId') courseId: string) {
-    return this.courseService.getTrackSessionsByCourseId(courseId);
-  }
-
-  // TODO: Add AuthGuard for check permission
-  @Get('crew/:courseId/sessions')
-  @ApiOperation({ summary: '특정 크루의 강의 세션 목록 조회' })
-  @ApiResponse({ status: 200, description: '성공', type: [CourseSessionDto] })
-  @ApiResponse({ status: 404, description: '크루를 찾을 수 없음' })
-  getCrewSessions(@Param('courseId') courseId: string) {
-    return this.courseService.getCrewSessionsByCourseId(courseId);
+  @Get('funding/:courseId')
+  @ApiOperation({ summary: '특정 펀딩 상세 조회' })
+  @ApiResponse({ status: 200, description: '성공', type: FundingCourseDto })
+  @ApiResponse({ status: 404, description: '펀딩을 찾을 수 없음' })
+  async getFundingByCourseId(@Param('courseId') courseId: string) {
+    const funding = await this.courseService.getFundingByCourseId(courseId);
+    if (!funding) {
+      throw new NotFoundException(`Funding with course ID ${courseId} not found`);
+    }
+    return funding;
   }
 }
