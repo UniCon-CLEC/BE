4c60d89b680fbdff37a59b90a3dd362ebbd1181f
Na1gae <nalgae@korea.ac.kr>
2025-09-06 02:33:15 +0900

    hotfix: Prisma 마이그레이션 히스토리 삭제

--- prisma/migrations/20250806054245_250806/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Tag" ADD COLUMN     "parentId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "public"."Tag" ADD CONSTRAINT "Tag_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "public"."Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
